f978425d2ef76ebcd2e29c2d9a0f2effc2e6321a
DanielOpazo <daniel.opazo.baer@gmail.com>
2016-03-03 17:25:33 -0500

    Added navigation bar at top of screen, and removed Live Feed header from page

--- stream.php
+++ stream.php
@@ -17,8 +17,31 @@ require('CameraFeed.php');
 		<script type='text/javascript' src='streamjs.js'></script>
 	</head>
 	<body>
+	<nav class="navbar navbar-inverse">
 	<div class="container-fluid">
-		<h1>Camera Feeds</h1>
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Video Surveillance System Dashboard</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a href="#">Home</a></li>
+			<li class="active"><a href="#">Live Feeds</a></li>
+			<li><a href="#">Add Feed</a></li> 
+			<li><a href="#">Remove Feed</a></li>
+			<li><a href="#">About</a></li>
+			<li class="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown" href="#">Careers
+				<span class="caret"></span></a>
+				<ul class="dropdown-menu">
+					<li><a href="#">Dropdown Menu Maker</a></li>
+					<li><a href="#">List Item Creator</a></li>
+					<li><a href="#">Caterer</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+	</nav>
+	
+	<div class="container-fluid">
 		<?php
 			$camerasList = getCamerasListFromFile();
 			$count = 0;
@@ -30,19 +53,16 @@ require('CameraFeed.php');
 					echo "<div class='row'>";
 					$temp = $count % 3;
 				}
-				echo "<div class='col-sm-4'><div class='bg-info'>".$feed->getName()."</div><div class='embed-responsive embed-responsive-4by3'><iframe src=".$feed->getUrl()."></iframe><div class=\"overlay\"></div></div></div>";
+				echo "<div class='col-sm-4'><div class='bg-info'>".$feed->getName()."</div><div class='well'><div class='embed-responsive embed-responsive-4by3'><iframe src=".$feed->getUrl()."></iframe><div class=\"overlay\"></div></div></div></div>";
 				$count++;
 			}
 			if ((($count) % 3) != 0) {
 				echo "</div>";
 			}
 		?>
-		<div class="row">
-			<div class="col-sm-6"><div id="newFeed"><a href=<?php $_SERVER["HTTP_HOST"];?>/newFeed.php>Add New Feed</a></div></div>
-			<div class="col-sm-6"><div id="removeFeed"><a href=<?php $_SERVER["HTTP_HOST"];?>/removeFeed.php>Remove Feed</a></div></div>
-		</div>
+		<a href=<?php $_SERVER["HTTP_HOST"];?>/newFeed.php class="btn btn-info" role="button">Add New Feed</a>
+		<a href=<?php $_SERVER["HTTP_HOST"];?>/removeFeed.php class="btn btn-danger" role="button">Remove Feed</a>
 </div>	
-	
 	
 </body>
 </html>
